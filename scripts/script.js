bdbb440ee6a877faee93387e8c80ecd7c04ff7e3
Madhuvantha K.A.N <37581397+Nawod@users.noreply.github.com>
2019-08-24 18:24:55 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//==============================================================================
+
+// How to load in modules
+const Diagnostics = require('Diagnostics');
+const Scene = require('Scene');
+const Patches = require('Patches');
+
+// How to access scene objects (uncomment line below to activate)
+ const directionalLight = Scene.root.find('directionalLight0');
+ var counterNumber = Scene.root.find('number');
+ var scoreNumber = Patches.getScalarValue('score');
+
+// How to access class properties (uncomment line below to activate)
+const directionalLightIntensity = directionalLight.intensity;
+counterNumber.text = scoreNumber.toString();
+// How to log messages to the console (uncomment line below to activate)
+// Diagnostics.log('Console message logged from the script.');
